ccb9cfa38793c0da35fa98696ae5c7a4c5a53d77
AbhirupChandra <abhirupchandra01work@gmail.com>
2023-07-14 22:34:22 +0530

    First Commit

---
+++ src/main/java/com/abhirupchandra/FinancialFreedom/FinancialFreedomApplication.java
@@ -0,0 +1,13 @@
+package com.abhirupchandra.FinancialFreedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialFreedomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialFreedomApplication.class, args);
+	}
+
+}
