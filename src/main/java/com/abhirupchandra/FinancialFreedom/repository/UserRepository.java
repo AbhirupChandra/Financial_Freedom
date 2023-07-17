5116f1a76951867dd969263932ec9df89a95d133
AbhirupChandra <abhirupchandra01work@gmail.com>
2023-07-18 00:13:05 +0530

    user repository push 1st

---
+++ src/main/java/com/abhirupchandra/FinancialFreedom/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.abhirupchandra.FinancialFreedom.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abhirupchandra.FinancialFreedom.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
